f026da98195b2d571d620ffd9134204502d56bbc
stefaleon <stefaleon@gmail.com>
2019-12-09 20:53:23 +0200

    force navigation from CommentBox to main route when logged out

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -5,6 +5,18 @@ import * as actions from "../actions";
 class CommentBox extends React.Component {
   state = { comment: "" };
 
+  componentDidMount() {
+    if (!this.props.auth) {
+      this.props.history.push("/");
+    }
+  }
+
+  componentDidUpdate() {
+    if (!this.props.auth) {
+      this.props.history.push("/");
+    }
+  }
+
   handleChange = e => {
     this.setState({ comment: e.target.value });
   };
@@ -32,4 +44,8 @@ class CommentBox extends React.Component {
   }
 }
 
-export default connect(null, actions)(CommentBox);
+const mapStateToProps = state => {
+  return { auth: state.auth };
+};
+
+export default connect(mapStateToProps, actions)(CommentBox);
