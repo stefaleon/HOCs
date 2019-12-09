2a7889ba7b3dd90a8f993c287db6bbc7ff198750
stefaleon <stefaleon@gmail.com>
2019-12-09 22:02:03 +0200

    HOC scaffold - DONT FORGET TO SPREAD THE PROPS THROUGH

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 export default ChildComponent => {
   class ComposedComponent extends Component {
     render() {
-      return <ChildComponent />;
+      return <ChildComponent {...this.props} />;
     }
   }
 
